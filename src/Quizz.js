b69a03333c2c5651d3b812a5ecb9644ed1164a9a
Igor Devisate <devisatei@gmail.com>
2023-08-18 21:00:12 -0300

    v0.9.2 just need to update restart

--- src/Quizz.js
+++ src/Quizz.js
@@ -7,37 +7,38 @@ export default function Quizz() {
 
     const [selectedAnswers, setSelectedAnwers] = useState([])
 
-    const [correctCount, setCorrectCount] = useState(0)
+    const [submitButtonClicked, setSubmitButtonClicked] = useState(false)
 
-    /*
     const shuffleArray = (array) => {
-        for (var i = array.length; i > 0; i--){
-            var j = Math.floor(Math.random() * (i + 1))
-            [array[i], array[j]] = [array[j], array[i]]
+        for (let i = array.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * (i + 1))
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
         return array
     }
-    */
 
     useEffect(() => {
         const fetchTriviaQuestions = async () => {
             try {
                 const res = await fetch('https://opentdb.com/api.php?amount=5&difficulty=easy')
                 const data = await res.json()
-                console.log(data)
                 
                 //shuffling answers for each question
                 const shuffledQuestions = data.results.map(questionData => {
                     const { question, incorrect_answers, correct_answer } = questionData
-                    //const answers = shuffleArray([...incorrect_answers, correct_answer])
+                    const answersArray = shuffleArray([...incorrect_answers, correct_answer])
                     return {
                         question: he.decode(question),
                         correct_answer: he.decode(correct_answer),
-                        answers: [...incorrect_answers, correct_answer]
+                        answers: answersArray.map(answer => he.decode(answer))
                     }
                 })
+                console.log(shuffledQuestions)
 
                 setQuestions(shuffledQuestions) 
+                setSelectedAnwers(new Array(shuffledQuestions.length).fill(null));
             } catch (error){
                 console.error("Questions not found: ", error)
             }
@@ -46,21 +47,20 @@ export default function Quizz() {
     }, [])
 
     function handleSelection(questionIndex, selectedAnswer) {
-        setSelectedAnwers(prevSelectedAnwers => {
-            const update = [...prevSelectedAnwers]
-            update[questionIndex] = selectedAnswer
-
-            if (selectedAnswer === questions[questionIndex].correct_answer) {
-                setCorrectCount(prevCounter => prevCounter + 1)
-            }
-
-            console.log(update)
-            return update 
-        })
+        if(!submitButtonClicked){
+            setSelectedAnwers(prevSelectedAnwers => {
+                const update = [...prevSelectedAnwers]
+                update[questionIndex] = selectedAnswer
+    
+                return update 
+            })
+        }
     }
 
     function handleSubmit(event) {
         event.preventDefault()
+        setSubmitButtonClicked(true)
+        showResults()
     }
 
     function showResults() {
@@ -87,10 +87,11 @@ export default function Quizz() {
 
     console.log(questions)
     console.log(selectedAnswers)
-    console.log(correctCount)
 
     return (
-        <main className="container">
+        <main className="container" style={{ position: 'relative' }}>
+            <div className="background-image-left"></div>
+            <div className="background-image-right"></div>
             <h1>Quiz Questions</h1>
                 <form onSubmit={handleSubmit}>
                 {questions.map((question, index) =>(
@@ -123,10 +124,15 @@ export default function Quizz() {
                             </div>
                         </div>
                 ))}
-                <button type="submit" className="submit-quizz-btn" onClick={() => showResults()}>Submit Answers</button>
+                <button 
+                type="submit" 
+                className="submit-quizz-btn" 
+                onClick={handleSubmit}
+                disabled={!selectedAnswers.every(answer => answer !== null)}
+                >Submit Answers</button>
             </form>
             <div id="result-div" className="hidden">
-                <h3>You got {correctCount/2} of {selectedAnswers.length} questions correct!</h3>
+                <h3>You got X of {selectedAnswers.length} questions correct!</h3>
                 <button className="play-again-btn">Play again.</button>
             </div>
         </main>
